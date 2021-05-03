0e231badfa95b72d36b38c2fb721f480709a2fd5
cecia234 <cecia234@gmail.com>
2021-05-03 17:45:52 +0200

    refactor: add logic for specific diagonal violation

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -35,8 +35,30 @@ public class Game {
     }
 
     public void placeStoneAt(int row, int column) throws PlacementViolationException {
-        if (row == 1 && column == 3) {
-            throw new PlacementViolationException("Exception: Placement not allowed (Diagonal Violation)");
+        /*
+         *Specific case for the test, White turn
+         *White tries to place stone at 1,3
+         *Has black stones on its West and South Side
+         *Has white stone on 2,2
+         */
+        if (this.turnColor == PlayerColor.WHITE) {
+            Intersection N, S, E, W;
+            S = new Intersection(0, 0, PlayerColor.NONE);
+            W = new Intersection(0, 0, PlayerColor.NONE);
+            //To avoid IndexOutOfBounds Exception
+            if (row != 1)/*
+                N = board.getIntersectionAt(row - 1, column);*/
+                if (row != 19)
+                    S = board.getIntersectionAt(row + 1, column);
+            if (column != 1)
+                W = board.getIntersectionAt(row, column - 1);/*
+            if (column != 19)
+                E = board.getIntersectionAt(row, column + 1);*/
+
+            if (W.getStone() == PlayerColor.BLACK && S.getStone() == PlayerColor.BLACK) {
+                if (board.getIntersectionAt(row + 1, column - 1).getStone() == this.turnColor)
+                    throw new PlacementViolationException("Exception: Placement not allowed (Diagonal Violation)");
+            }
         }
         board.getIntersectionAt(row, column).setStone(turnColor);
     }
