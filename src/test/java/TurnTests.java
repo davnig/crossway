fdfc1fe5b6070a27842cd2e983c7996f8cf0067f
cecia234 <cecia234@gmail.com>
2021-05-06 13:30:08 +0200

    test: add whenPlayerPlacesStoneAtPosition33AndNEDiagonalViolationShouldOutputError

--- src/test/java/TurnTests.java
+++ src/test/java/TurnTests.java
@@ -7,38 +7,55 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class TurnTests {
 
 
-	@Test
-	void isIntersectionEmpty() {
-		Intersection intersection = new Intersection(1, 1, PlayerColor.NONE);
-		assertEquals(intersection.getStone(), PlayerColor.NONE);
-	}
+    @Test
+    void isIntersectionEmpty() {
+        Intersection intersection = new Intersection(1, 1, PlayerColor.NONE);
+        assertEquals(intersection.getStone(), PlayerColor.NONE);
+    }
 
-	@Test
-	void whenPlayerPlacesStoneAtPosition13AndDiagonalViolationShouldOutputError() {
-		Board presetBoard = new Board();
+    //test south-west violation
+    @Test
+    void whenPlayerPlacesStoneAtPosition13AndSWDiagonalViolationShouldOutputError() {
+        Board presetBoard = new Board();
 
-		presetBoard.getIntersectionAt(1, 2).setStone(PlayerColor.BLACK);
-		presetBoard.getIntersectionAt(2, 3).setStone(PlayerColor.BLACK);
-		presetBoard.getIntersectionAt(2, 2).setStone(PlayerColor.WHITE);
+        presetBoard.getIntersectionAt(1, 2).setStone(PlayerColor.BLACK);
+        presetBoard.getIntersectionAt(2, 3).setStone(PlayerColor.BLACK);
+        presetBoard.getIntersectionAt(2, 2).setStone(PlayerColor.WHITE);
 
-		Game game = new Game(presetBoard);
-		game.setTurnColor(PlayerColor.WHITE);
+        Game game = new Game(presetBoard);
+        game.setTurnColor(PlayerColor.WHITE);
 
-		assertThrows(PlacementViolationException.class, () -> game.placeStoneAt(1, 3));
-	}
-	@Test
-	void whenPlayerPlacesStoneAtPosition24AndDiagonalViolationShouldOutputError() {
-		Board presetBoard = new Board();
+        assertThrows(PlacementViolationException.class, () -> game.placeStoneAt(1, 3));
+    }
 
-		presetBoard.getIntersectionAt(1, 4).setStone(PlayerColor.BLACK);
-		presetBoard.getIntersectionAt(2, 3).setStone(PlayerColor.BLACK);
-		presetBoard.getIntersectionAt(1, 3).setStone(PlayerColor.WHITE);
+    //test north-west violation
+    @Test
+    void whenPlayerPlacesStoneAtPosition24AndNWDiagonalViolationShouldOutputError() {
+        Board presetBoard = new Board();
 
-		Game game = new Game(presetBoard);
-		game.setTurnColor(PlayerColor.WHITE);
+        presetBoard.getIntersectionAt(1, 4).setStone(PlayerColor.BLACK);
+        presetBoard.getIntersectionAt(2, 3).setStone(PlayerColor.BLACK);
+        presetBoard.getIntersectionAt(1, 3).setStone(PlayerColor.WHITE);
 
-		assertThrows(PlacementViolationException.class, () -> game.placeStoneAt(2, 4));
-	}
+        Game game = new Game(presetBoard);
+        game.setTurnColor(PlayerColor.WHITE);
 
+        assertThrows(PlacementViolationException.class, () -> game.placeStoneAt(2, 4));
+    }
+
+    //test north-east violation
+    @Test
+    void whenPlayerPlacesStoneAtPosition33AndNEDiagonalViolationShouldOutputError() {
+        Board presetBoard = new Board();
+
+        presetBoard.getIntersectionAt(3, 4).setStone(PlayerColor.BLACK);
+        presetBoard.getIntersectionAt(2, 3).setStone(PlayerColor.BLACK);
+        presetBoard.getIntersectionAt(2, 4).setStone(PlayerColor.WHITE);
+
+        Game game = new Game(presetBoard);
+        game.setTurnColor(PlayerColor.WHITE);
+
+        assertThrows(PlacementViolationException.class, () -> game.placeStoneAt(3, 3));
+    }
 
 }
