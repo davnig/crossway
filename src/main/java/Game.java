2b5b00d271e7923ba12f15aa2b62ef6cba04eb7b
cecia234 <cecia234@gmail.com>
2021-05-07 16:42:11 +0200

    refactor: -add global variables to check for Pie Rule -add pieRule method -add playTurn method

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,6 +5,8 @@ import lombok.Data;
 public class Game {
 
     private Board board;
+    private int currentTurn;
+    private boolean hasPieRuleAlreadyBeenUsed = false;
     private PlayerColor turnColor;
 
 
@@ -16,19 +18,30 @@ public class Game {
         this.board = presetBoard;
     }
 
+    Game(Board presetBoard, int turn) {
+        this.board = presetBoard;
+        this.currentTurn = turn;
+    }
 
     void start() {
         this.initFirstTurn();
         System.out.println("Nuova partita avviata.\nInizia il nero.");
     }
 
-    public void placeStoneAt(int row, int column) throws PlacementViolationException {
+    public String playTurn() {
+        if (currentTurn == 2) {
+            if (!hasPieRuleAlreadyBeenUsed) {
+                return pieRule();
+            }
+        }
+        return "It's just another turn.";
+    }
 
+    public void placeStoneAt(int row, int column) throws PlacementViolationException {
         if (board.isLastMoveDiagonalViolation(row, column, turnColor, getOppositePlayerColor()))
             throw new PlacementViolationException("Placement not allowed: diagonal violation");
 
         board.getIntersectionAt(row, column).setStone(turnColor);
-
     }
 
     public void switchTurn() {
@@ -45,11 +58,18 @@ public class Game {
 
     private void initFirstTurn() {
         setTurnColor(PlayerColor.BLACK);
+        currentTurn = 1;
     }
 
     private PlayerColor getOppositePlayerColor() {
         return isWhiteTurn() ? PlayerColor.BLACK : PlayerColor.WHITE;
     }
 
+    private String pieRule() {
+        //here you should ask a player whether or not he/she wants to switch colors
+        //implementation depends on future choices, now returns a string just to see if game responds
+        hasPieRuleAlreadyBeenUsed = true;
+        return "Pie Rule!";
+    }
 }
 
