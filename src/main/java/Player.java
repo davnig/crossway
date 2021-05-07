18d95c8886178774b256c61008c707f7c68fd51f
cecia234 <cecia234@gmail.com>
2021-05-07 18:59:02 +0200

    refactor: add constructors and annotations

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,6 +1,8 @@
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Player {
     private PlayerID id;
     private PlayerColor color;
--- src/main/java/Turn.java
+++ src/main/java/Turn.java
@@ -1,7 +1,12 @@
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Turn {
     private int currentTurn;
     private Player currentPlayer;
+
+    Turn() {
+    }
 }
