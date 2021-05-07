52f24cbe5b79a9c99f9c0f8f018498549bdbdb84
heapify00 <daddets97@gmail.com>
2021-05-07 11:39:59 +0200

    refactor: generalize diagonal violation check

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -34,4 +34,104 @@ public class Board {
                 .get(0);
 
     }
+
+    PlayerColor getStoneColorAt(int row, int column) {
+        return getIntersectionAt(row, column).getStone();
+    }
+
+    boolean isLastMoveDiagonalViolation(int row, int column, PlayerColor turnColor, PlayerColor oppositeColor) {
+
+        if (isFirstRowPosition(row))
+            return isSouthDiagonalViolation(row, column, turnColor, oppositeColor);
+
+        if (isLastRowPosition(row))
+            return isNorthDiagonalViolation(row, column, turnColor, oppositeColor);
+
+        if (isFirstColumnPosition(column))
+            return isEastDiagonalViolation(row, column, turnColor, oppositeColor);
+
+        if (isLastColumnPosition(column))
+            return isWestDiagonalViolation(row, column, turnColor, oppositeColor);
+
+        return
+                isNorthDiagonalViolation(row, column, turnColor, oppositeColor) ||
+                        isSouthDiagonalViolation(row, column, turnColor, oppositeColor) ||
+                        isWestDiagonalViolation(row, column, turnColor, oppositeColor) ||
+                        isEastDiagonalViolation(row, column, turnColor, oppositeColor);
+
+
+    }
+
+
+    private boolean isWestDiagonalViolation(int row, int column, PlayerColor turnColor, PlayerColor oppositeColor) {
+
+        if (getStoneColorAt(row, column + 1) != oppositeColor)
+            return false;
+
+        if (getStoneColorAt(row - 1, column) == oppositeColor
+                && getStoneColorAt(row - 1, column + 1) == turnColor)
+            return true;
+
+        return getStoneColorAt(row + 1, column) == oppositeColor
+                && getStoneColorAt(row + 1, column + 1) == turnColor;
+
+    }
+
+    private boolean isEastDiagonalViolation(int row, int column, PlayerColor turnColor, PlayerColor oppositeColor) {
+
+        if (getStoneColorAt(row, column - 1) != oppositeColor)
+            return false;
+
+        if (getStoneColorAt(row - 1, column) == oppositeColor
+                && getStoneColorAt(row - 1, column - 1) == turnColor)
+            return true;
+
+        return getStoneColorAt(row + 1, column) == oppositeColor
+                && getStoneColorAt(row + 1, column - 1) == turnColor;
+
+    }
+
+    private boolean isNorthDiagonalViolation(int row, int column, PlayerColor turnColor, PlayerColor oppositeColor) {
+
+        if (getStoneColorAt(row - 1, column) != oppositeColor)
+            return false;
+
+        if (getStoneColorAt(row, column - 1) == oppositeColor
+                && getStoneColorAt(row - 1, column - 1) == turnColor)
+            return true;
+
+        return getStoneColorAt(row, column + 1) == oppositeColor
+                && getStoneColorAt(row - 1, column + 1) == turnColor;
+
+    }
+
+    private boolean isSouthDiagonalViolation(int row, int column, PlayerColor turnColor, PlayerColor oppositeColor) {
+
+        if (getStoneColorAt(row + 1, column) != oppositeColor)
+            return false;
+
+        if (getStoneColorAt(row, column - 1) == oppositeColor
+                && getStoneColorAt(row + 1, column - 1) == turnColor)
+            return true;
+
+        return getStoneColorAt(row, column + 1) == oppositeColor
+                && getStoneColorAt(row + 1, column + 1) == turnColor;
+
+    }
+
+    private boolean isLastColumnPosition(int column) {
+        return column == MAX_COLUMN;
+    }
+
+    private boolean isFirstColumnPosition(int column) {
+        return column == 1;
+    }
+
+    private boolean isLastRowPosition(int row) {
+        return row == getMAX_ROW();
+    }
+
+    private boolean isFirstRowPosition(int row) {
+        return row == 1;
+    }
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -35,57 +35,20 @@ public class Game {
     }
 
     public void placeStoneAt(int row, int column) throws PlacementViolationException {
-        /*
-         *Specific case for the test, White turn
-         *White tries to place stone at 1,3
-         *Has black stones on its West and South Side
-         *Has white stone on 2,2
-         */
-        if (isWhiteTurn()) {
-            if (isLastMoveDiagonalViolationFromWhite(row, column)) {
-                throw new PlacementViolationException("Exception: Placement not allowed (Diagonal Violation)");
-            } else {
-                board.getIntersectionAt(row, column).setStone(turnColor);
-            }
-        } else {
-            board.getIntersectionAt(row, column).setStone(turnColor);
-        }
+
+        if (board.isLastMoveDiagonalViolation(row, column, turnColor, getOppositePlayerColor()))
+            throw new PlacementViolationException("Placement not allowed: diagonal violation");
+
+        board.getIntersectionAt(row, column).setStone(turnColor);
+
     }
 
-    private boolean isLastMoveDiagonalViolationFromWhite(int row, int column) {
-        Intersection northIntersection = null, southIntersection = null, eastIntersection = null, westIntersection = null;
+    private PlayerColor getOppositePlayerColor() {
+        if (isWhiteTurn())
+            return PlayerColor.BLACK;
 
-        //To avoid IndexOutOfBounds Exception
-        if (row != 1) {
-            northIntersection = board.getIntersectionAt(row - 1, column);
-        }
-        if (row != 19) {
-            southIntersection = board.getIntersectionAt(row + 1, column);
-        }
-        if (column != 1) {
-            westIntersection = board.getIntersectionAt(row, column - 1);
-        }
-        if (column != 19) {
-            eastIntersection = board.getIntersectionAt(row, column + 1);
-        }
-
-        if (westIntersection != null && southIntersection != null) { //south-west violation i.e.
-            if (westIntersection.getStone() == PlayerColor.BLACK && southIntersection.getStone() == PlayerColor.BLACK)
-                return board.getIntersectionAt(row + 1, column - 1).getStone() == this.turnColor;
-        }
-        if (northIntersection != null && westIntersection != null) { //north-west violation i.e.
-            if (northIntersection.getStone() == PlayerColor.BLACK && westIntersection.getStone() == PlayerColor.BLACK)
-                return board.getIntersectionAt(row - 1, column - 1).getStone() == this.turnColor;
-        }
-        if (northIntersection != null && eastIntersection != null) { //north-east violation i.e.
-            if (northIntersection.getStone() == PlayerColor.BLACK && eastIntersection.getStone() == PlayerColor.BLACK)
-                return board.getIntersectionAt(row - 1, column + 1).getStone() == this.turnColor;
-        }
-        if (eastIntersection != null && southIntersection != null) { //south-east violation i.e.
-            if (eastIntersection.getStone() == PlayerColor.BLACK && southIntersection.getStone() == PlayerColor.BLACK)
-                return board.getIntersectionAt(row + 1, column + 1).getStone() == this.turnColor;
-        }
-        return false;
+        return PlayerColor.WHITE;
     }
+
 }
 
