24066941c813d71cc3f6e3533cda10c8e3011f88
heapify00 <daddets97@gmail.com>
2021-10-22 11:04:07 +0200

    test(client): add failing tests

--- client/src/test/java/it/units/crossway/client/GameHandlerTests.java
+++ client/src/test/java/it/units/crossway/client/GameHandlerTests.java
@@ -4,13 +4,16 @@ import it.units.crossway.client.model.PlayerColor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class GameHandlerTests {
 
     @Test
     void whenGameStartsBoardShouldBeEmpty() {
         GameHandler gameHandler = new GameHandler();
         gameHandler.getTurn().initFirstTurn();
-        Assertions.assertTrue(gameHandler.getBoard().getBoardState().isEmpty());
+        assertTrue(gameHandler.getBoard().getBoardState().isEmpty());
     }
 
     @Test
@@ -18,6 +21,54 @@ public class GameHandlerTests {
         GameHandler gameHandler = new GameHandler();
         gameHandler.getTurn().initFirstTurn();
         Assertions.assertEquals(PlayerColor.BLACK, gameHandler.getTurn().getCurrentPlayer());
+    }
+
+    @Test
+    void whenPlayerInsertsNicknameShouldSendAddPlayerReq() {
+        // TODO
+        fail();
+    }
+
+    @Test
+    void whenPlayerSelectsNewGameShouldSendCreateGameReq() {
+        // TODO
+        fail();
+    }
+
+    @Test
+    void whenPlayerSelectsEnterGameShouldSendGetAvailableGamesReq() {
+        // TODO
+        fail();
+    }
+
+    @Test
+    void whenPlayerSelectsAvailableGameShouldSendJoinGameReq() {
+        // TODO
+        fail();
+    }
+
+    @Test
+    void whenCreateGameAndGameStartsThenPlayerShouldBeBlack() {
+        // TODO
+        fail();
+    }
+
+    @Test
+    void whenJoinGameAndGameStartsThenPlayerShouldBeWhite() {
+        // TODO
+        fail();
+    }
+
+    @Test
+    void whenPlayerPlaysTurnShouldSendStonePlacementIntent() {
+        // TODO
+        fail();
+    }
+
+    @Test
+    void whenStonePlacementIntentIsReceivedThenBoardShouldBeUpdated() {
+        // TODO
+        fail();
     }
 
 }
