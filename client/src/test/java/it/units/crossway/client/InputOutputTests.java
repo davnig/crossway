8913c8e3d55c187187f659e092fc2161432463f9
MatteoScorcia <matteoscorcia@gmail.com>
2021-10-30 17:16:02 +0200

    test(client): add test print board 10x10

--- client/src/test/java/it/units/crossway/client/InputOutputTests.java
+++ client/src/test/java/it/units/crossway/client/InputOutputTests.java
@@ -40,4 +40,32 @@ public class InputOutputTests {
         board.resetBoard();
     }
 
+    @Test
+    void print10x10EmptyBoard() {
+        String board10x10 =
+                        "   ----------------------------------------- " + System.lineSeparator() +
+                        "1  |   |   |   |   |   |   |   |   |   |   | " + System.lineSeparator() +
+                        "2  |   |   |   |   |   |   |   |   |   |   | " + System.lineSeparator() +
+                        "3  |   |   |   |   |   |   |   |   |   |   | " + System.lineSeparator() +
+                        "4  |   |   |   |   |   |   |   |   |   |   | " + System.lineSeparator() +
+                        "5  |   |   |   |   |   |   |   |   |   |   | " + System.lineSeparator() +
+                        "6  |   |   |   |   |   |   |   |   |   |   | " + System.lineSeparator() +
+                        "7  |   |   |   |   |   |   |   |   |   |   | " + System.lineSeparator() +
+                        "8  |   |   |   |   |   |   |   |   |   |   | " + System.lineSeparator() +
+                        "9  |   |   |   |   |   |   |   |   |   |   | " + System.lineSeparator() +
+                        "10 |   |   |   |   |   |   |   |   |   |   | " + System.lineSeparator() +
+                        "   ----------------------------------------- " + System.lineSeparator() +
+                        "     1   2   3   4   5   6   7   8   9   10  " + System.lineSeparator();
+
+        ByteArrayOutputStream baos = IOUtils.redirectSystemOutToByteArrayOS();
+
+        Board board = new Board();
+        Board.LAST_COLUMN = 10;
+        Board.LAST_ROW = 10;
+        IOUtils.printBoard(board);
+        String printedBoard = baos.toString();
+        assertEquals(board10x10, printedBoard);
+        board.resetBoard();
+    }
+
 }
